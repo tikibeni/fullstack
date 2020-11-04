fe48ecaf48dd787ad547d108efd204d0ddd07859
Benjamin Blinnikka <benjamin.blinnikka@hotmail.com>
2020-11-04 18:58:12 +0200

    Tehtävät 4.13-4.14

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -6,6 +6,15 @@ blogsRouter.get('/', async (request, response) => {
     response.json(blogs.map(blog => blog.toJSON()))
 })
 
+blogsRouter.get('/:id', async (request, response) => {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+        response.json(blog.toJSON())
+    } else {
+        response.status(404).end()
+    }
+})
+
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
@@ -28,12 +37,25 @@ blogsRouter.post('/', async (request, response) => {
     }
 })
 
-blogsRouter.get('/:id', async (request, response) => {
-    const blog = await Blog.findById(request.params.id)
-    if (blog) {
-        response.json(blog.toJSON())
+blogsRouter.put('/:id', async (request, response) => {
+    const body = request.body
+
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+    }
+
+    if (blog.likes === undefined) {
+        blog.likes = 0
+    }
+
+    if (blog.title === (undefined || '') && blog.url === (undefined || '')) {
+        response.status(400).end()
     } else {
-        response.status(404).end()
+        const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog)
+        response.json(updatedBlog.toJSON())
     }
 })
 
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -5,107 +5,208 @@ const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
 
-beforeEach(async () => {
-    await Blog.deleteMany({})
-    await Blog.insertMany(helper.initialBlogs)
+describe('Routetesting', () => {
+    beforeEach(async () => {
+        await Blog.deleteMany({})
+        await Blog.insertMany(helper.initialBlogs)
+    })
+    
+    describe('General GET-Method', () => {
+        test('returns blogs as json', async () => {
+            await api
+              .get('/api/blogs')
+              .expect(200)
+              .expect('Content-Type', /application\/json/)
+        })
+        
+        test('returns correct amount of blogs', async () => {
+            const response = await api.get('/api/blogs')
+        
+            expect(response.body).toHaveLength(helper.initialBlogs.length)
+        })
+        
+        test('returns specific blogtitle from db', async () => {
+            const response = await api.get('/api/blogs')
+        
+            const titles = response.body.map(r => r.title)
+            expect(titles).toContain(
+                'Full-Stack Development 101'
+            )
+        })
+        
+        test('returned blogs are identified by id', async () => {
+            const response = await api.get('/api/blogs')
+        
+            const ids = response.body.map(r => r.id)
+            expect(ids).toBeDefined()
+        })
+    })
+
+    describe('Specific GET-Method', () => {
+        test('GET with valid id works correctly', async () => {
+            const blogsAtStart = await helper.blogsInDatabase()
+
+            const blogToView = blogsAtStart[0]
+
+            const resultBlog = await api
+              .get(`/api/blogs/${blogToView.id}`)
+              .expect(200)
+              .expect('Content-Type', /application\/json/)
+
+            const jsonblogToView = JSON.parse(JSON.stringify(blogToView))
+
+            expect(resultBlog.body).toEqual(jsonblogToView)
+        })
+
+        test('GET with non-existing id returns 404', async () => {
+            const nonExistingId = await helper.nonExistingId()
+
+            await api
+              .get(`/api/blogs/${nonExistingId}`)
+              .expect(404)
+        })
+    })
+
+    describe('POST-Method', () => {
+        test('valid blog can be added', async () => {
+            const newBlog = {
+                title: 'Node Testing 101',
+                author: 'FS-Team',
+                url: 'https://fullstackopen.com',
+                likes: 5000
+            }
+        
+            await api
+              .post('/api/blogs')
+              .send(newBlog)
+              .expect(200)
+              .expect('Content-Type', /application\/json/)
+        
+            const blogsAtEnd = await helper.blogsInDatabase()
+            expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+        
+            const titles = blogsAtEnd.map(r => r.title)
+        
+            expect(titles).toContain(
+                'Node Testing 101'
+            )
+        })
+        
+        test('blog post without defined likes returns zero likes', async () => {
+            const newBlog = {
+                title: 'TTK91',
+                author: 'Tito',
+                url: 'Nope'
+            }
+        
+            await api
+              .post('/api/blogs')
+              .send(newBlog)
+              .expect(200)
+              .expect('Content-Type', /application\/json/)
+        
+            const blogsAtEnd = await helper.blogsInDatabase()
+            expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+        
+            const addedBlog = blogsAtEnd.find(blog => blog.title === newBlog.title)
+            expect(addedBlog.likes).toBe(0)
+        })
+        
+        test('blog post without defined title and url returns error 400', async () => {
+            const newBlog = {
+                author: 'Mr. Error'
+            }
+        
+            await api
+              .post('/api/blogs')
+              .send(newBlog)
+              .expect(400)
+        
+            const blogsAtEnd = await helper.blogsInDatabase()
+            expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+        })
+    })
+
+    describe('DELETE-method', () => {
+        test('method returns status code 204 if id is valid', async () => {
+            const blogsAtStart = await helper.blogsInDatabase()
+            const blogToDelete = blogsAtStart[0]
+
+            await api
+              .delete(`/api/blogs/${blogToDelete.id}`)
+              .expect(204)
+
+            const blogsAtEnd = await helper.blogsInDatabase()
+
+            expect(blogsAtEnd).toHaveLength(
+                helper.initialBlogs.length - 1
+            )
+
+            const titles = blogsAtEnd.map(r => r.title)
+
+            expect(titles).not.toContain(blogToDelete.title)
+        })
+    })
+
+    describe('PUT-method', () => {
+        test('updating blog with correct id works correctly', async () => {
+            const blogsAtStart = await helper.blogsInDatabase()
+
+            const blogToUpdate = blogsAtStart[0]
+            blogToUpdate.author = 'P=NP'
+
+            await api
+              .put(`/api/blogs/${blogToUpdate.id}`)
+              .send(blogToUpdate)
+              .expect(200)
+              .expect('Content-Type', /application\/json/)
+
+            const blogsAtEnd = await helper.blogsInDatabase()
+
+            expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+
+            const authors = blogsAtEnd.map(r => r.author)
+
+            expect(authors).toContain('P=NP')
+        }) 
+
+        test('updating blog to contain no url and title leads to error', async () => {
+            const blogsAtStart = await helper.blogsInDatabase()
+
+            const blogToUpdate = blogsAtStart[0]
+            blogToUpdate.title = ''
+            blogToUpdate.url = ''
+
+            await api
+              .put(`/api/blogs/${blogToUpdate.id}`)
+              .send(blogToUpdate)
+              .expect(400)
+
+            const blogsAtEnd = await helper.blogsInDatabase()
+            const titles = blogsAtEnd.map(r => r.title)
+
+            expect(titles).not.toContain('')
+        })
+
+        test('updating likes works as intended', async () => {
+            const blogsAtStart = await helper.blogsInDatabase()
+
+            const blogToUpdate = blogsAtStart[0]
+            blogToUpdate.likes = 99928264528
+
+            await api
+              .put(`/api/blogs/${blogToUpdate.id}`)
+              .send(blogToUpdate)
+              .expect(200)
+
+            const blogsAtEnd = await helper.blogsInDatabase()
+            const likes = blogsAtEnd.map(r => r.likes)
+
+            expect(likes).toContain(blogToUpdate.likes)
+        })
+    })
 })
 
-// GET-ALL-LOHKO
-test('returns blogs as json', async () => {
-    await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
-})
-
-test('returns correct amount of blogs', async () => {
-    const response = await api.get('/api/blogs')
-
-    expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
-
-test('returns specific blog from db', async () => {
-    const response = await api.get('/api/blogs')
-
-    const titles = response.body.map(r => r.title)
-    expect(titles).toContain(
-        'Full-Stack Development 101'
-    )
-})
-
-test('returned blogs are identified by id', async () => {
-    const response = await api.get('/api/blogs')
-
-    const ids = response.body.map(r => r.id)
-    expect(ids).toBeDefined()
-})
-// GET-ALL-LOHKO
-
-// POST-LOHKO
-test('valid blog can be added', async () => {
-    const newBlog = {
-        title: 'Node Testing 101',
-        author: 'FS-Team',
-        url: 'https://fullstackopen.com',
-        likes: 5000
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
-
-    const blogsAtEnd = await helper.blogsInDatabase()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-    const titles = blogsAtEnd.map(r => r.title)
-
-    expect(titles).toContain(
-        'Node Testing 101'
-    )
-})
-
-test('blog post without defined likes returns zero likes', async () => {
-    const newBlog = {
-        title: 'TTK91',
-        author: 'Tito',
-        url: 'Nope'
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
-
-    const blogsAtEnd = await helper.blogsInDatabase()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-    const addedBlog = blogsAtEnd.find(blog => blog.title === newBlog.title)
-    expect(addedBlog.likes).toBe(0)
-})
-
-test('blog post without defined title and url returns error 400', async () => {
-    const newBlog = {
-        author: 'Mr. Error'
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
-
-    const blogsAtEnd = await helper.blogsInDatabase()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
-})
-// POST-LOHKO
-
-// DELETE-LOHKO
-
-
-
-// DELETE-LOHKO
 
 afterAll(() => {
     mongoose.connection.close()
