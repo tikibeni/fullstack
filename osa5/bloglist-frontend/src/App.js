7e83e54a56e68621c8f4bcfa2e61cbc41a3fbcf4
Benjamin Blinnikka <benjamin.blinnikka@hotmail.com>
2020-11-16 16:11:41 +0200

    Tehtävä 5.2

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -11,19 +11,36 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
+  // Hakee suoraan kaikki blogit kannasta
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
 
+  // Tarkistaa sivun avaamisen yhteydessä onko käyttäjä jo kirjautunut
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  // Käsittelee kirjautumisprosessin
   const handleLogin = async (event) => {
     event.preventDefault()
-
     try {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedUser', JSON.stringify(user)
+      )
+
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -33,6 +50,13 @@ const App = () => {
         setErrorMessage(null)
       }, 5000)
     }
+  }
+
+  const handleLogout = (event) => {
+    event.preventDefault()
+    window.localStorage.removeItem('loggedUser')
+    setUser(null)
+    blogService.setToken(null)
   }
 
   const loginForm = () => (
@@ -59,7 +83,6 @@ const App = () => {
     </form>
   )
 
-
   return (
     <div>
       <Notification message={errorMessage} />
@@ -74,7 +97,10 @@ const App = () => {
 
         <div>
           <h1>blogs</h1>
-          <p>{user.username} logged in</p>
+          <p>{user.username} logged in</p> 
+
+          <button type="submit" onClick={handleLogout}>logout</button>
+
           <ul>
             {blogs.map(blog => 
               <Blog key={blog.id} blog={blog} />
