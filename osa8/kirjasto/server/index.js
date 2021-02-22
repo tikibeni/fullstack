7e8199831c458914d4d6a37b7cc1fa7b2563473b
Benjamin Blinnikka <benjamin.blinnikka@hotmail.com>
2021-02-22 16:13:09 +0200

    Tehtävä 8.6

--- osa8/kirjasto/server/index.js
+++ osa8/kirjasto/server/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { v1: uuid } = require('uuid')
 
 let authors = [
     {
@@ -105,6 +106,20 @@ const typeDefs = gql`
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+  
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+    
+    addAuthor(
+      name: String!
+      born: Int
+    ): Author
+  }
 `
 
 const resolvers = {
@@ -133,8 +148,30 @@ const resolvers = {
     },
     Author: {
         bookCount: (root) => books.filter(book => book.author === root.name).length
-    }
+    },
 
+    Mutation: {
+        addBook: (root, args) => {
+            const book = { ...args, id: uuid() }
+            books = books.concat(book)
+
+            if (!authors.find(a => a.name === book.author)) {
+                const author = { name: book.author, id: uuid() }
+                authors = authors.concat(author)
+            }
+
+            return book
+        },
+
+        /*
+        addAuthor: (root, args) => {
+            const author = {...args, id: uuid()}
+            authors = authors.concat(author)
+            return author
+        }
+
+         */
+    }
 }
 
 const server = new ApolloServer({
