ee87631b80f05c623bbcfe91217b6d340b1c544c
Benjamin Blinnikka <benjamin.blinnikka@hotmail.com>
2021-07-23 15:15:56 +0300

    Tehtävä 9.18.

--- osa9/patientor/src/state/reducer.ts
+++ osa9/patientor/src/state/reducer.ts
@@ -36,3 +36,21 @@ export const reducer = (state: State, action: Action): State => {
       return state;
   }
 };
+
+export const setPatientList = (payload: Patient[]) => {
+  const action: Action = {
+    type: "SET_PATIENT_LIST",
+    payload
+  };
+
+  return action;
+};
+
+export const addPatient = (payload: Patient) => {
+  const action: Action = {
+    type: "ADD_PATIENT",
+    payload
+  };
+
+  return action;
+};
